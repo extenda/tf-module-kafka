e049dafd3b1e3294c90f316b2ddd5a5b87e3f639
Anastasiia Kovtun <anastasiia.kovtun@extendaretail.com>
2022-04-28 12:43:41 +0100

    feat: Switch from ccloud to confluent CLI

--- kafka-connect-ccloud/ccloud.py
+++ kafka-connect-ccloud/ccloud.py
@@ -10,12 +10,12 @@ import tarfile
 import_existing_connection = False
 
 
-def download_ccloud():
-    if not os.path.exists('ccloud/ccloud'):
-        url = 'https://s3-us-west-2.amazonaws.com/confluent.cloud/ccloud-cli/archives/latest/ccloud_latest_%s_amd64.tar.gz' % sys.platform
-        os.system('curl -o ccloud.tar.gz %s' % url)
-        os.system('tar -xzf ccloud.tar.gz ccloud/ccloud')
-        os.remove('ccloud.tar.gz')
+def download_confluent():
+    if not os.path.exists('confluent/confluent'):
+        url = 'https://s3-us-west-2.amazonaws.com/confluent.cloud/confluent-cli/archives/latest/confluent_latest_%s_amd64.tar.gz' % sys.platform
+        os.system('curl -o confluent.tar.gz %s' % url)
+        os.system('tar -xzf confluent.tar.gz confluent/confluent')
+        os.remove('confluent.tar.gz')
 
 
 def create_connection_config():
@@ -30,17 +30,17 @@ def delete_connection_config():
     os.remove('connection.config')
 
 
-def ccloud_login(login, password):
-    testcmd = subprocess.run('./ccloud/ccloud connector list', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8', timeout=60)
-    if 'Error: not logged in' in testcmd.stderr:
-        logincmd = subprocess.Popen('timeout 60 ./ccloud/ccloud login --prompt --no-browser', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=0, encoding='utf-8')
+def confluent_login(login, password):
+    testcmd = subprocess.run('./confluent/confluent connect list', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8', timeout=60)
+    if 'Error: you must log in to Confluent Cloud' in testcmd.stderr:
+        logincmd = subprocess.Popen('timeout 60 ./confluent/confluent login --prompt --no-browser', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=0, encoding='utf-8')
         logincmd.communicate('%s\n%s\n' % (login, password))
         if logincmd.returncode != 0:
             sys.exit(logincmd.returncode)
 
 
 def get_real_connection_config(environment, cluster, id):
-    cmd = subprocess.run('./ccloud/ccloud connector describe --environment %s --cluster %s %s -o json' % (environment, cluster, id), shell=True, stdout=subprocess.PIPE, encoding='utf-8', timeout=60)
+    cmd = subprocess.run('./confluent/confluent connect describe --environment %s --cluster %s %s -o json' % (environment, cluster, id), shell=True, stdout=subprocess.PIPE, encoding='utf-8', timeout=60)
     if cmd.returncode != 0:
         sys.exit(cmd.returncode)
     config = {}
@@ -52,7 +52,7 @@ def get_real_connection_config(environment, cluster, id):
 
 
 def get_existing_connection_id(environment, cluster, name):
-    cmd = subprocess.run('./ccloud/ccloud connector list --environment %s --cluster %s -o json' % (environment, cluster), shell=True, stdout=subprocess.PIPE, encoding='utf-8', timeout=60)
+    cmd = subprocess.run('./confluent/confluent connect list --environment %s --cluster %s -o json' % (environment, cluster), shell=True, stdout=subprocess.PIPE, encoding='utf-8', timeout=60)
     connectors = json.loads(cmd.stdout)
     for connector in connectors:
         if connector['name'] == name:
@@ -61,7 +61,7 @@ def get_existing_connection_id(environment, cluster, name):
 # ------------ terraform commands ------------
 
 def create():
-    ccloud_login(os.environ['CONFLUENT_USERNAME'], os.environ['CONFLUENT_PASSWORD'])
+    confluent_login(os.environ['CONFLUENT_USERNAME'], os.environ['CONFLUENT_PASSWORD'])
 
     environment = os.environ['CONFLUENT_ENVIRONMENT']
     cluster = os.environ['CONFLUENT_CLUSTER']
@@ -69,7 +69,7 @@ def create():
     id = get_existing_connection_id(environment, cluster, os.environ['CONNECTION_name']) if import_existing_connection else None
     if not id:
         create_connection_config()
-        cmd = subprocess.run('./ccloud/ccloud connector create --environment %s --cluster %s --config connection.config' % (environment, cluster), shell=True, stdout=subprocess.PIPE, encoding='utf-8', timeout=300)
+        cmd = subprocess.run('./confluent/confluent connect create --environment %s --cluster %s --config connection.config' % (environment, cluster), shell=True, stdout=subprocess.PIPE, encoding='utf-8', timeout=300)
         delete_connection_config()
         if cmd.returncode != 0:
             sys.exit(cmd.returncode)
@@ -80,20 +80,20 @@ def create():
 
 
 def delete():
-    ccloud_login(os.environ['CONFLUENT_USERNAME'], os.environ['CONFLUENT_PASSWORD'])
+    confluent_login(os.environ['CONFLUENT_USERNAME'], os.environ['CONFLUENT_PASSWORD'])
 
     previous_output = json.loads(sys.stdin.read())
     environment = os.environ['CONFLUENT_ENVIRONMENT']
     cluster = os.environ['CONFLUENT_CLUSTER']
     id = previous_output['id']
 
-    cmd = subprocess.run('./ccloud/ccloud connector delete --environment %s --cluster %s %s' % (environment, cluster, id), shell=True, stdout=subprocess.PIPE, encoding='utf-8', timeout=300)
+    cmd = subprocess.run('./confluent/confluent connect delete --environment %s --cluster %s %s' % (environment, cluster, id), shell=True, stdout=subprocess.PIPE, encoding='utf-8', timeout=300)
     if cmd.returncode != 0:
         sys.exit(cmd.returncode)
 
 
 def read():
-    ccloud_login(os.environ['CONFLUENT_USERNAME'], os.environ['CONFLUENT_PASSWORD'])
+    confluent_login(os.environ['CONFLUENT_USERNAME'], os.environ['CONFLUENT_PASSWORD'])
 
     previous_output = json.loads(sys.stdin.read())
     environment = os.environ['CONFLUENT_ENVIRONMENT']
@@ -105,7 +105,7 @@ def read():
 
 
 def update():
-    ccloud_login(os.environ['CONFLUENT_USERNAME'], os.environ['CONFLUENT_PASSWORD'])
+    confluent_login(os.environ['CONFLUENT_USERNAME'], os.environ['CONFLUENT_PASSWORD'])
 
     previous_output = json.loads(sys.stdin.read())
     environment = os.environ['CONFLUENT_ENVIRONMENT']
@@ -113,7 +113,7 @@ def update():
     id = previous_output['id']
 
     create_connection_config()
-    cmd = subprocess.run('./ccloud/ccloud connector update --environment %s --cluster %s --config connection.config %s' % (environment, cluster, id), shell=True, stdout=subprocess.PIPE, encoding='utf-8', timeout=300)
+    cmd = subprocess.run('./confluent/confluent connect update --environment %s --cluster %s --config connection.config %s' % (environment, cluster, id), shell=True, stdout=subprocess.PIPE, encoding='utf-8', timeout=300)
     delete_connection_config()
     if cmd.returncode != 0:
         sys.exit(cmd.returncode)
@@ -123,7 +123,7 @@ def update():
 
 
 if __name__=='__main__':
-    download_ccloud()
+    download_confluent()
     if len(sys.argv) > 1:
         if sys.argv[1]=='create':
             create()
